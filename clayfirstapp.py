e1678536087b9040c50e8c5fd3d232a35a7deda2
claywrentz <cwrentz@smu.edu>
2022-04-13 16:14:40 -0500

    Added comment feature

--- clayfirstapp.py
+++ clayfirstapp.py
@@ -15,3 +15,18 @@ st.button('click me', on_click=increment_counter, kwargs=dict(increment_value=1)
 st.button('dont click me', on_click=decrement_counter, kwargs=dict(decrement_value=1))
 
 st.write(st.session_state.count)
+
+user_input = st.text_area("Leave a comment!")
+file1 = open("comments.txt", "a")
+file1.write(user_input)
+file1.close()
+
+file1 = open("comments.txt", "r")
+comments_list = file1.readlines()
+
+st.write('Comments: ')
+
+if comments_list:
+	for x in comments_list:
+		st.write(x)
+
